7e87a06df77f211b57ad435d161fd9c46ed6e7ef
bilguunZorigooLeap1 <bilguunzorigooleap1@iMac2021-02s-iMac.local>
2022-09-27 11:47:41 +0800

    converted to useState

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
   const [pixels, setPixels] = useState(new Map());
-  const [gameArray,setGameArray] = useState(Array(30).fill(Array(30).fill(0)))
-  const [snakePosition, setSnakePosition] = useState([[0,0],[0,1],[0,2],[0,3],[0,4]]);
-  const [currentSnakeKeys, setCurrentSnakeKeys] = useState(toPosSet(snakePosition));
-  const [isChanging, setIsChanging] = useState(true);
-  // const [currentDirection, setCurrentDirection] = useState("");
+  const [gameArray, setGameArray] = useState(Array(30).fill(Array(30).fill(0)));
+  const [snakePosition, setSnakePosition] = useState([
+    [0, 0],
+    [0, 1],
+    [0, 2],
+    [0, 3],
+    [0, 4],
+  ]);
+  const [currentSnakeKeys, setCurrentSnakeKeys] = useState(
+    toPosSet(snakePosition)
+  );
 
   const moveRight = ([x, y]) => [x, y + 1];
   const moveLeft = ([x, y]) => [x, y - 1];
   const moveUp = ([x, y]) => [x - 1, y];
   const moveDown = ([x, y]) => [x + 1, y];
-
-
+  // const [currentDirection, setCurrentDirection] = useState(() => moveRight);
+  let currentDirection = moveDown
   function toPosSet(position) {
     let set = new Set();
     for (let cell of position) {
@@ -24,16 +30,12 @@ function App() {
     return set;
   }
 
-  function toKey([x,y]) {
-    return x + '_' + y
+  function toKey([x, y]) {
+    return x + "_" + y;
   }
 
   function Row({ children }) {
-    return (
-      <div style={{ display: 'flex', width: `100%` }}>
-        {children}
-      </div>
-    )
+    return <div style={{ display: "flex", width: `100%` }}>{children}</div>;
   }
 
   function Pixel({ pixelPosition }) {
@@ -43,64 +45,62 @@ function App() {
           width: `10px`,
           height: `10px`,
           border: `1px solid #aaa`,
-          background: currentSnakeKeys.has(pixelPosition)
-          ? 'black' : 'none'
-        }}></div>
+          background: currentSnakeKeys.has(pixelPosition) ? "black" : "none",
+        }}
+      ></div>
     );
   }
-  
-  let currentDirection = moveRight;
-  document.addEventListener('keydown', (e) => {
+
+  document.addEventListener("keydown", (e) => {
     switch (e.key) {
       case "ArrowDown":
-        currentDirection = moveDown;
+      currentDirection=moveDown
+        // setCurrentDirection(() => moveDown);
         break;
       case "ArrowUp":
-        currentDirection = moveUp;
+      currentDirection=moveUp
+        // setCurrentDirection(() => moveUp);
         break;
       case "ArrowLeft":
-        currentDirection = moveLeft;
+      currentDirection=moveLeft
+        // setCurrentDirection(() => moveLeft);
         break;
       case "ArrowRight":
-        currentDirection = moveRight;
+      currentDirection=moveRight
+        // setCurrentDirection(() => moveRight);
         break;
     }
- })
+  });
   function step() {
-    
-
-
-    setSnakePosition(prevVal => {
+    setSnakePosition((prevVal) => {
       let prevValAcopy = prevVal;
       prevValAcopy.shift();
       let head = snakePosition[snakePosition.length - 1];
-      
-
-      // let newHead = [head[0], head[1] + 1];
       let newHead = currentDirection(head);
       prevValAcopy.push(newHead);
       setCurrentSnakeKeys(toPosSet(prevValAcopy));
       return prevValAcopy;
-    })
+    });
   }
   useEffect(() => {
-    let gameInterval = setInterval(step, 2000);
+    let gameInterval = setInterval(step, 1000);
     return () => clearInterval(gameInterval);
-  },[snakePosition])
-  
+  }, [snakePosition]);
   return (
     <div className="canvas">
       {gameArray.map((row, i) => {
         return (
-          <Row key={row+i}>
+          <Row key={row + i}>
             {row.map((rowChild, j) => {
               let position = i + "_" + j;
               pixels.set(position, <div></div>);
-              return <Pixel key={rowChild+j} pixelPosition={position}></Pixel>;
+              return (
+                <Pixel key={rowChild + j} pixelPosition={position}></Pixel>
+              );
             })}
           </Row>
         );
-       })}
+      })}
     </div>
   );
 }
--- src/game.md
+++ src/game.md
@@ -1,1 +1,8 @@
- 
+const a = useCustomHook('direction',newHead);
+e
+function useCustomHook('direction',newHead) {
+    if(direction === "ArrowUp") {
+    	return changedNewHead
+    }
+
+} 
